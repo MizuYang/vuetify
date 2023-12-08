97f1858e9f5737cb6c84d8f9d6aae758f9bc22a8
Mizu <psdk55052004@gmail.com>
2023-12-08 15:36:57 +0800

    修改 將 Demo2 滾動區塊調小，避免往下滑時一直滾動到此區塊

--- src/components/demo1/A2.vue
+++ src/components/demo1/A2.vue
@@ -2,7 +2,7 @@
   <h4 class="text-18 mb-5">向下/向上滾動到該元素，背景色變白色</h4>
 
   <!-- 滾動容器 -->
-  <div class="w-100"
+  <div class="w-25 text-center mx-auto"
        :class="bgColor"
        style="max-height:300px;overflowY:scroll">
     <div style="height:850px;">
@@ -10,7 +10,7 @@
            style="width:100%;
                   height:140px;margin-top:450px;
                   background-color:red;">
-        <p v-intersect="onIntersect" style="font-size:80px;">進入可視範圍，背景變灰</p>
+        <p v-intersect="onIntersect" style="font-size:30px;">進入可視範圍，背景變灰</p>
       </div>
     </div>
   </div>
